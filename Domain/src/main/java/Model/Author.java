ff95dc2ba59b8f25fc2619e40806d350928f491c
Jacek Mucha <jacekmucha@gmail.com>
2018-10-20 10:43:57 +0200

    Author Model prepared

---
+++ Domain/src/main/java/Model/Author.java
@@ -0,0 +1,22 @@
+package Model;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+
+@Entity
+@Table (name = "author")
+public class Author {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private BigInteger id;
+
+    @Column (name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column (name = "last_name", nullable = false)
+    private String lastName;
+
+    private String birthPlace;
+
+}
